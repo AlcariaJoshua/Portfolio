edb323d73155800c801e0a3e3a2629ab269f99d0
AlcariaJoshua <alcariajoshua13@gmail.com>
2023-05-08 15:46:16 +0800

    mobile done

---
+++ dist/script.js
@@ -0,0 +1,27 @@
+const burger_btn = document.querySelector('.burger_btn');
+const nav = document.querySelector("nav");
+
+burger_btn.addEventListener('click', () => {
+    burger_btn.classList.toggle('open');
+    nav.classList.toggle('open');
+});
+
+
+//slider interest
+  var slider = tns({
+    container: '.slider',
+    items: 2,
+    slideBy: 'page',
+    autoplay: true,
+    controls: false,
+    gutter: 16
+  });
+
+  //slider projects
+  var slider = tns({
+    container: '.project-slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    controls: false
+  });
